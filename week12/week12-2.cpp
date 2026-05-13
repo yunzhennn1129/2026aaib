fe7c0f803917701280fe8a46ce221cd3e93c165e
yunzhennn1129 <14750694@me.mcu.edu.tw>
2026-05-13 11:36:45 +0800

    week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,15 @@
+//week12-2.cpp SOIT106_ADVANCE_008_C
+#include <stdio.h>
+int f(int n)
+{
+	if(n<0) return -n;
+	return n;
+}
+
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+	printf("[%d]",f(n));
+	return 0;
+}
